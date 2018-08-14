e781cc9328978550a5aefbae69fafb33b4d1f976
JoaoMarcs <joaomarcospereira1515@gmail.com>
2018-08-14 09:38:16 -0300

    Timer

--- Assets/scripts/GM.cs
+++ Assets/scripts/GM.cs
@@ -16,6 +16,11 @@ public class GM : MonoBehaviour {
 
 	public float timeToRespawn = 2f;
 
+	public float maxTime = 120f;
+	bool timerOn = true;
+
+	float timeLeft;
+
 	public UI ui;
 
 	GameData data = new GameData();
@@ -31,6 +36,7 @@ public class GM : MonoBehaviour {
 		if (player == null){
 			RespawnPlayer();
 		}
+		timeLeft = maxTime;
 	}
 	
 	// Update is called once per frame
@@ -41,11 +47,24 @@ public class GM : MonoBehaviour {
 				player = obj.GetComponent<PlayerCtrl>();
 			}
 		}
+		UpdateTimer();
 		DisplayHudData();
+	}
+
+	void UpdateTimer(){
+		if(timerOn){
+			timeLeft = timeLeft - Time.deltaTime;
+			if(timeLeft <= 0f){
+				timeLeft = 0;
+				ExpirePlayer();
+			}
+
+		}
 	}
 
 	void DisplayHudData(){
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtTimer.text = "Timer : " + timeLeft.ToString("F1");
 	}
 	public void IncrementCoinCount(){
 		data.coinCount++;
@@ -61,5 +80,21 @@ public class GM : MonoBehaviour {
 			Invoke("RespawnPlayer", timeToRespawn);
 		}
 
+	}
+
+	public void ExpirePlayer(){
+		if (player != null ){
+			Destroy(player.gameObject);
+			
+		}
+		GameOver();
+
+	}
+
+	void GameOver(){
+		timerOn = false;
+		ui.gameOver.txtCoinCount.text = "Coins: " + data.coinCount;
+		ui.gameOver.txtTimer.text = "Timer : " + timeLeft.ToString("F1");
+		ui.gameOver.gameOverPanel.SetActive(true);
 	}
 }
